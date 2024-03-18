31067cd64a22bc392d574bc71f5edc4d8ae9cc27
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-03-18 14:31:42 +0700

    release to user

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 require 'vendor/autoload.php';
-if( isset($_POST['authKey'])&& ($_POST['authKey']=='abc'))
-{
-//
-    $stripe = new \Stripe\StripeClient('sk_test_51OsiPhJSl4kaFANTv2e7tiGqrAWMTuwfpotd45nWI6YeCbboGeLAykIfpfv4MycQTuHuXIto5r32SFZxaDgnKEYf00MBzOYfdE'); // secret key
 
-// Use an existing Customer ID if this is a returning customer.
+if (isset($_POST['authKey']) && ($_POST['authKey'] == 'abc')) {
+    // Lấy thông tin tên người dùng và giá của Manga Plus App từ yêu cầu POST
+    $userName = isset($_POST['userName']) ? $_POST['userName'] : '';
+    $amount = isset($_POST['amount']) ? $_POST['amount'] : '';
+
+    // Khởi tạo Stripe với secret key
+    $stripe = new \Stripe\StripeClient('sk_test_51OsiPhJSl4kaFANTv2e7tiGqrAWMTuwfpotd45nWI6YeCbboGeLAykIfpfv4MycQTuHuXIto5r32SFZxaDgnKEYf00MBzOYfdE');
+
+    // Tạo khách hàng Stripe với tên người dùng và địa chỉ mặc định
     $customer = $stripe->customers->create([
-        'name' => "HoangUyen",
+        'name' => $userName,
         'address' => [
             'line1' => 'demo address',
             'postal_code' => '738933',
@@ -16,36 +20,35 @@ if( isset($_POST['authKey'])&& ($_POST['authKey']=='abc'))
             'country' => 'US',
         ]
     ]);
+
+    // Tạo ephemeral key cho khách hàng với phiên bản API Stripe được chỉ định
     $ephemeralKey = $stripe->ephemeralKeys->create([
         'customer' => $customer->id,
     ], [
         'stripe_version' => '2023-10-16',
     ]);
+
+    // Tạo payment intent với số tiền và thông tin khách hàng đã cung cấp
     $paymentIntent = $stripe->paymentIntents->create([
-        'amount' => 1099,
+        'amount' => $amount,
         'currency' => 'usd',
         'description' => 'Payment for MangaPlus App',
         'customer' => $customer->id,
-        // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-        // is optional because Stripe enables its functionality by default.
         'automatic_payment_methods' => [
             'enabled' => 'true',
         ],
     ]);
 
-    // Thiết lập phản hồi HTTP trước khi gửi dữ liệu đến trình duyệt
+    // Thiết lập phản hồi HTTP và gửi dữ liệu JSON về máy khách
     http_response_code(200);
-
-    // Gửi dữ liệu JSON đến trình duyệt
-    echo json_encode(
-        [
-            'paymentIntent' => $paymentIntent->client_secret,
-            'ephemeralKey' => $ephemeralKey->secret,
-            'customer' => $customer->id,
-            'publishableKey' => 'pk_test_51OsiPhJSl4kaFANTuqY3Tw9yPOIqUMaPwkFbuKvbLjCeQC0njrHdH810sCyqbcK2Im1IZOK9euVvSu8gRjZcCEgE002RvYJJ5h'
-        ]
-    );
-}
-else{
+    echo json_encode([
+        'paymentIntent' => $paymentIntent->client_secret,
+        'ephemeralKey' => $ephemeralKey->secret,
+        'customer' => $customer->id,
+        'publishableKey' => 'pk_test_51OsiPhJSl4kaFANTuqY3Tw9yPOIqUMaPwkFbuKvbLjCeQC0njrHdH810sCyqbcK2Im1IZOK9euVvSu8gRjZcCEgE002RvYJJ5h'
+    ]);
+} else {
+    // Trả về thông báo không được ủy quyền nếu authKey không hợp lệ
     echo "Not authorised";
 }
+?>
